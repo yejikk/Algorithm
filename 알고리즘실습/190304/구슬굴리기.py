158c175ac05949b0b60ce738ddcf0c9ad8d90d23
unknown <dpwl7484@gmail.com>
2019-03-04 22:42:45 +0900

    190304 | 알고리즘실습

--- 알고리즘실습/190304/구슬굴리기.py
+++ 알고리즘실습/190304/구슬굴리기.py
@@ -58,5 +58,3 @@ while True:
         break
 
 print(cnt)
-
-
--- 알고리즘실습/190304/스파이조직.py
+++ 알고리즘실습/190304/스파이조직.py
@@ -3,3 +3,15 @@ sys.stdin = open('스파이조직.txt')
 
 N, spy = map(str, input().split())
 print(spy)
+
+secret = []
+num = ''
+for name in spy:
+    if name != '<' and name != '>':
+        num += name
+    else:
+        if num != '':
+            secret.append(num)
+            num = ''
+        secret.append(name)
+print(secret)
